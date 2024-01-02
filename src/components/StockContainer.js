58ccebe1bb7b0206002009d3d7fdcf71b8ecd26d
Tony Eder <tonyeder11@gmail.com>
2024-01-02 10:10:26 -0800

    stocks rendering in v.Dom -  no errors

--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({ stocks }) {
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stocks.map((stock) => (
+        <Stock key={stock.id} name={stock.name} price={stock.price} ticker={stock.ticker} type={stock.type} />
+      ))}
+      
     </div>
   );
 }
